28202a2f9ae37d901b3b5346b0d67db59ee30c03
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2022-08-25 19:07:11 +0300

    More tests

--- tests/by-util/test_chr.rs
+++ tests/by-util/test_chr.rs
@@ -2,5 +2,21 @@ use crate::common::util::*;
 
 #[test]
 fn test_chr() {
-    new_ucmd!().succeeds();
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
+}
+
+#[test]
+fn test_chr2() {
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
+}
+
+#[test]
+fn test_chr3() {
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
 }
