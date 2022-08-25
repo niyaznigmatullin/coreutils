baad96f5f587a9eff7e6c22528cb7aa42236ede2
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2022-08-25 19:35:52 +0300

    Add test

--- src/uu/chr/src/chr.rs
+++ src/uu/chr/src/chr.rs
@@ -2,9 +2,28 @@ use chrono::{DateTime, Local};
 use clap::Command;
 use uucore::error::UResult;
 
+
+fn file_last_modified_time(path: &str) -> String {
+    std::fs::metadata(path)
+        .map(|i| {
+            i.modified()
+                .map(|x| {
+                    let date_time: DateTime<Local> = x.into();
+                    date_time.format("%b %d %H:%M %Y").to_string()
+                })
+                .unwrap_or_default()
+        })
+        .unwrap_or_default()
+}
+
 #[uucore::main]
-pub fn uumain(_args: impl uucore::Args) -> UResult<()> {
-    println!("{}", get_time());
+pub fn uumain(args: impl uucore::Args) -> UResult<()> {
+    let args = args.collect_ignore();
+    if args.len() < 2 {
+        println!("{}", get_time());
+    } else {
+        println!("{}", file_last_modified_time(&args[1]));
+    }
     Ok(())
 }
 
--- tests/by-util/test_chr.rs
+++ tests/by-util/test_chr.rs
@@ -20,3 +20,12 @@ fn test_chr3() {
         new_ucmd!().succeeds();
     }
 }
+
+#[test]
+fn test_metadata() {
+    for _ in 0..100 {
+        let (at, mut ucmd) = at_and_ucmd!();
+        at.touch("hey");
+        ucmd.arg("hey").succeeds();
+    }
+}
