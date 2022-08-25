5eff0992f983a7243b6bb3f24fb3cc533c710bcc
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2022-08-25 20:04:02 +0300

    more tests

--- tests/by-util/test_chr.rs
+++ tests/by-util/test_chr.rs
@@ -22,6 +22,69 @@ fn test_chr3() {
 }
 
 #[test]
+fn test_chr11() {
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
+}
+
+
+#[test]
+fn test_chr4() {
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
+}
+
+
+#[test]
+fn test_chr5() {
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
+}
+
+
+#[test]
+fn test_chr6() {
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
+}
+
+
+#[test]
+fn test_chr7() {
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
+}
+
+
+#[test]
+fn test_chr8() {
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
+}
+
+
+#[test]
+fn test_chr9() {
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
+}
+
+
+#[test]
+fn test_chr10() {
+    for _ in 0..100 {
+        new_ucmd!().succeeds();
+    }
+}
+
+#[test]
 fn test_metadata() {
     for _ in 0..100 {
         let (at, mut ucmd) = at_and_ucmd!();
