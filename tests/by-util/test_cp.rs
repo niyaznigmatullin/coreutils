9acd2f0c8122b8967cb4fb1b35b4544024c8bb90
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2022-09-09 10:46:02 +0300

    Test1

--- tests/by-util/test_cp.rs
+++ tests/by-util/test_cp.rs
@@ -1380,26 +1380,26 @@ fn test_cp_reflink_insufficient_permission() {
 #[test]
 fn test_closes_file_descriptors() {
     use procfs::process::Process;
-    let me = Process::myself().unwrap();
+    let _me = Process::myself().unwrap();
 
-    // The test suite runs in parallel, we have pipe, sockets
-    // opened by other tests.
-    // So, we take in account the various fd to increase the limit
-    let number_file_already_opened: u64 = me.fd_count().unwrap().try_into().unwrap();
-    let limit_fd: u64 = number_file_already_opened + 9;
+    // // The test suite runs in parallel, we have pipe, sockets
+    // // opened by other tests.
+    // // So, we take in account the various fd to increase the limit
+    // let number_file_already_opened: u64 = me.fd_count().unwrap().try_into().unwrap();
+    // let limit_fd: u64 = number_file_already_opened + 9;
 
-    // For debugging purposes:
-    for f in me.fd().unwrap() {
-        let fd = f.unwrap();
-        println!("{:?} {:?}", fd, fd.mode());
-    }
+    // // For debugging purposes:
+    // for f in me.fd().unwrap() {
+    //     let fd = f.unwrap();
+    //     println!("{:?} {:?}", fd, fd.mode());
+    // }
 
     new_ucmd!()
         .arg("-r")
         .arg("--reflink=auto")
         .arg("dir_with_10_files/")
         .arg("dir_with_10_files_new/")
-        .with_limit(Resource::NOFILE, limit_fd, limit_fd)
+        .with_limit(Resource::NOFILE, 9, 9)
         .succeeds();
 }
 
